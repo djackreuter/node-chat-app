e7c1735f512d85d1586dc42c6af66c1568646c07
Jack Reuter <djreuter45@gmail.com>
2018-08-02 09:57:22 -0600

    set up express and package configs

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+app.listen(3000, () => console.log(`Started server on port ${port}...`));
