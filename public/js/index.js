54a7ee8c1152f320c7872593766b186dd57c61d6
Jack Reuter <djreuter45@gmail.com>
2018-08-02 15:14:47 -0600

    emit new message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,6 @@ var socket = io();
 socket.on('connect', function() {
   console.log('connected to server');
 
-  socket.emit('createMessage', {
-    to: 'test@test.com',
-    text: 'test message body'
-  });
 });
 socket.on('disconnect', function() {
   console.log('disconnected from server');
--- server/server.js
+++ server/server.js
@@ -15,14 +15,12 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('new user connected');
 
-  socket.emit('newMessage', {
-    from: 'Jimmy',
-    text: 'Test message',
-    createdAt: new Date().toString()
-  });
-
   socket.on('createMessage', (message) => {
-    console.log('new message from client', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    })
   });
   
   socket.on('disconnect', () => {
