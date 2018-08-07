12cbccdb6017d814eda2edb29071c27535b68921
Jack Reuter <djreuter45@gmail.com>
2018-08-07 09:29:21 -0600

    added form for sending messages and displaying messages

--- public/js/index.js
+++ public/js/index.js
@@ -9,4 +9,18 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
   console.log('got new message from server', message);
+  var li = $('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+  $('#messages').append(li);
+});
+
+
+$('#message-form').on('submit', function(e) {
+  e.preventDefault();
+  socket.emit('createMessage', {
+    from: 'Jimmy',
+    text: $('[name=message]').val()
+  }, function(data) {
+    console.log('Got it', data);
+  });
 });
