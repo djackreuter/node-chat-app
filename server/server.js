203b9bb2b3652c7d3265ed0bc379620df5ceae44
Jack Reuter <djreuter45@gmail.com>
2018-08-11 17:09:03 -0600

    added users to app

--- server/server.js
+++ server/server.js
@@ -5,18 +5,34 @@ const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const {generateMessage, generateLocationMessage} = require('./utils/message');
+const {isRealString} = require('./utils/validation');
+const Users = require('./utils/users');
 const port = process.env.PORT || 3000;
 
 const app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
+var users = new Users();
 
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-  console.log('new user connected');
-  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app!'));
-  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined!'));
+  
+  socket.on('join', (params, callback) => {
+    if (!isRealString(params.name) || !isRealString(params.room)) {
+      return callback('Name and room name are required');
+    }
+    socket.join(params.room);
+    users.removeUser(socket.id);
+    users.addUser(socket.id, params.name, params.room);
+
+    io.to(params.room).emit('updateUserList', users.getUserList(params.room));
+
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app!'));
+    socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined!`));
+
+    callback();
+  });
 
   socket.on('createMessage', (message, callback) => {
     io.emit('newMessage', generateMessage(message.from, message.text));
@@ -24,7 +40,11 @@ io.on('connection', (socket) => {
   });
   
   socket.on('disconnect', () => {
-    console.log('client disconnected');
+    var user = users.removeUser(socket.id);
+    if (user) {
+      io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+      io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left`));
+    }
   });
 
   socket.on('createLocationMessage', (coords) => {
