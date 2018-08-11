84e90abcc6fc0aec1d13f1e5c5118b3137dcebd9
Jack Reuter <djreuter45@gmail.com>
2018-08-11 17:22:45 -0600

    made messages private per room

--- public/js/chat.js
+++ public/js/chat.js
@@ -64,7 +64,6 @@ socket.on('newLocationMessage', function(message) {
 $('#message-form').on('submit', function(e) {
   e.preventDefault();
   socket.emit('createMessage', {
-    from: 'Jimmy',
     text: $('[name=message]').val()
   }, function() {
     $('[name=message]').val('');
